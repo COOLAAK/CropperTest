2b686409a63c5f5cbb0cb15a98c46d53313a4563
Сафнюк Богдаан <bodyasafff@gmail.com>
2019-08-30 14:12:49 +0300

    Add project files.

---
+++ WebApplication4/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ContentResult UploadImage(string croppedImage)
+        {
+            string filePath = String.Empty;
+
+            string base64 = croppedImage;
+            byte[] bytes = Convert.FromBase64String(base64.Split(',')[1]);
+            Bitmap bmp;
+            using (var ms = new MemoryStream(bytes))
+            {
+                bmp = new Bitmap(ms);
+            }
+            string path = Server.MapPath("/Images/");
+            Bitmap imageBig = Utils.CreateImage(bmp, 360, 480);
+            filePath = path + Guid.NewGuid() + ".png";
+            imageBig.Save(filePath, ImageFormat.Jpeg);
+            string json = JsonConvert.SerializeObject(filePath);
+            return Content(json, "application/json");
+
+        }
+    }
+}
---
+++ WebApplication4/Models/Utils.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication4.Models
+{
+    public class Utils
+    {
+        public static Bitmap CreateImage(Bitmap originalPic, int maxWidth, int maxHeight)
+        {
+            //Перевірка існування файла
+
+            // Вычисление новых размеров картинки
+            int width = originalPic.Width; //текущая ширина
+            int height = originalPic.Height; //текущая высота
+            int widthDiff = (width - maxWidth); //разница с допуст. шириной
+            int heightDiff = (height - maxHeight); //разница с допуст. высотой
+                                                   // Определение размеров, которые необходимо изменять
+            bool doWidthResize = (maxWidth > 0 && width > maxWidth &&
+                                widthDiff > -1 && widthDiff > heightDiff);
+            bool doHeightResize = (maxHeight > 0 && height > maxHeight &&
+                                heightDiff > -1 && heightDiff > widthDiff);
+            // Ресайз картинки
+            if (doWidthResize || doHeightResize || (width.Equals(height)
+                            && widthDiff.Equals(heightDiff)))
+            {
+                int iStart;
+                Decimal divider;
+                if (doWidthResize)
+                {
+                    iStart = width;
+                    divider = Math.Abs((Decimal)iStart / maxWidth);
+                    width = maxWidth;
+                    height = (int)Math.Round((height / divider));
+                }
+                else
+                {
+                    iStart = height;
+                    divider = Math.Abs((Decimal)iStart / maxHeight);
+                    height = maxHeight;
+                    width = (int)Math.Round((width / divider));
+                }
+            }
+            //Використання интерполяции
+            using (System.Drawing.Bitmap outBmp = new System.Drawing.Bitmap(width, height, PixelFormat.Format16bppRgb555))
+            {
+                using (System.Drawing.Graphics oGraphics = System.Drawing.Graphics.FromImage(outBmp))
+                {
+                    oGraphics.DrawImage(originalPic, 0, 0, width, height);
+                    return new Bitmap(outBmp);
+                }
+            }
+        }
+    }
+}
